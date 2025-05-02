c216d7b9d00eb54b69110d7f23fc82c8b5fc9a0a
DerickJoel-dev <derickdev2@gmail.com>
2025-05-02 19:42:30 -0400

    Simple helper function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -17,4 +17,4 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
         page (int): page number
         page_size (int): size of the page
     """
-    return ((page - 1) * page_size * page_size)
+    return ((page - 1) * page_size, page * page_size)
